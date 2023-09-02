3ec87aaff6d04bbcdd1038e440b4f769d967922d
khyleB <116310302+khyleB@users.noreply.github.com>
2023-09-02 15:43:24 -0400

    Made sun draggable

---
+++ src/components/SunsetPrimary.js
@@ -0,0 +1,19 @@
+
+
+/*import { Component } from "react";
+
+class SunsetPrimary extends Component {
+
+    render(){
+        const {colour} = this.props;
+        var sunsetColour = '#F1F1F1';
+        
+        return <div id="sun" backgroundColor={colour}>
+
+          <div id="sky-colour-primary" style={backgroundImage=`linear-gradient(to bottom, ${color}80, #00000000)`} />
+
+      </div>
+    }
+}
+
+export default SunsetPrimary;*/
